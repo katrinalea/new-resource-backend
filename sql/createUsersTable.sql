9180d21bf27c4c3672e3e3b81d9547c67da93f38
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-17 13:54:25 +0000

    added line to populate users

--- sql/createUsersTable.sql
+++ sql/createUsersTable.sql
@@ -5,3 +5,5 @@ CREATE TABLE  users (
     user_name           varchar(50),
     faculty_status      boolean
 );
+
+INSERT INTO users (user_name, faculty_status) VALUES ('Katrina', false),('Josiah', false),('Maria', false),('Zac', false);
